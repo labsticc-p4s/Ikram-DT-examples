a983d54a0c6ca3c71f3f11f12e692fc71543551e
ikramYSF <ikram.yousfi@imt-atlantique.fr>
2026-03-31 12:23:18 +0200

    bioreactor example

---
+++ Bioreactor/bioreactor-mock-service/src/main/java/com/bioreactordt/bioreactormockservice/BioreactorMockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bioreactordt.bioreactormockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioreactorMockServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BioreactorMockServiceApplication.class, args);
+    }
+
+}
---
+++ Bioreactor/bioreactor-mock-service/src/test/java/com/bioreactordt/bioreactormockservice/BioreactorMockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bioreactordt.bioreactormockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioreactorMockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
