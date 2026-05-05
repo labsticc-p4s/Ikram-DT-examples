ab417818f55aa07c72f983d2e7c15a9fd8151654
Ikram Yousfi <ikram.yousfi@imt-atlantique.fr>
2026-05-05 11:37:39 +0200

    Delete Bioreactor directory

--- Bioreactor/models-service/src/main/java/com/bioreactordt/modelsservice/calculators/temperature/TempGammaFunction.java
+++
@@ -1,6 +0,0 @@
-package com.bioreactordt.modelsservice.calculators.temperature;
-
-public interface TempGammaFunction {
-    double calculate(double temp, double tempMin, double tempOpt, double tempMax);
-
-}
